f70544ac37d9a3f65628974189cd49db4cc78968
Brad Erickson <eosrei@gmail.com>
2017-04-15 23:10:16 -0700

    add: Parse CLI args and exists(input_file)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
-extern crate png;
+// File Visualizer
+// 2017 Brad Erickson
+// https://github.com/eosrei/file-vis
 
+extern crate png;
+extern crate clap;
+
+use std::process::exit;
 use std::path::Path;
+use std::io::Write;
 use std::fs::File;
 use std::io::BufWriter;
 
+use clap::{Arg, App};
+
 fn main() {
+    // Setup stderr
+    let mut stderr = std::io::stderr();
+
+    // Parse cli args
+    let matches = App::new("File Visualizer")
+        .version("1.0")
+        .author("Brad Erickson https://github.com/eosrei/file-vis")
+        .about("Create PNG images from binary files")
+        .arg(Arg::with_name("INPUT")
+                 .help("Sets the input file")
+                 .required(true)
+                 .index(1))
+        .get_matches();
+
+    // Read the input file
+    let input_file = matches.value_of("INPUT").unwrap();
+    let path = Path::new(input_file);
+    if !path.exists() {
+        writeln!(&mut stderr, "File not found: {}", input_file).unwrap();
+        exit(1);
+    }
+
+    // Find height/width of square ratio image
+    // Pad file byte array to fit image
+    // Find RGB color for each byte
+
     // To use encoder.set()
     use png::HasParameters;
 
@@ -12,13 +47,12 @@ fn main() {
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
 
-    let mut encoder = png::Encoder::new(w, 2, 1); // Width is 2 pixels and height is 1.
+    let mut encoder = png::Encoder::new(w, 2, 1);
     encoder
-        .set(png::ColorType::RGBA)
+        .set(png::ColorType::Grayscale)
         .set(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
 
-    // An array containing a RGBA sequence. First pixel is red and second pixel is black.
-    let data = [255, 0, 0, 255, 0, 0, 0, 255];
-    writer.write_image_data(&data).unwrap(); // Save
+    let data = [255, 255];
+    writer.write_image_data(&data).unwrap();
 }
